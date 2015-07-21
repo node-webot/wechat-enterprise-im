286f1c7efc26312ad8d76d4877df46c38702c282
马剑 <majian@gisiinfo.com>
2015-07-21 15:49:01 +0800

    建立结构

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wechat-enterprise-im
 A lib for wechat enterprise IM services.
+
+微信公共平台企业号版－企业号消息
+
+## 官方暂时未释放。
---
+++ index.js
@@ -0,0 +1,5 @@
+var wechat_im = require('./lib/wechat-enterprise-im');
+// wechat.List = require('./lib/list');
+// wechat.API = require('wechat-enterprise-api');
+// wechat.util = require('./lib/util');
+module.exports = wechat_im;
